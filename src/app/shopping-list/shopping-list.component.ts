4cff35a2bfc805de51cd4801bcf58e6c2a3dcf76
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2020-06-17 01:05:13 -0700

    merge all the components togetther

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'shopping-list',
+    selector: 'app-shopping-list',
     templateUrl: './shopping-list.component.html',
     styleUrls: ['./shopping-list.component.css']
 })
